375fe165f1b9c8c479e8182386b46dcdf1fa6d8d
muhammadsidd <muhammadsidd@mail.usf.edu>
2021-03-31 12:31:38 -0400

    interface component created for service module

--- src/FeastFreedom/src/app/service/service.module.ts
+++ src/FeastFreedom/src/app/service/service.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { InterfaceComponent } from './interface/interface.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [InterfaceComponent],
   imports: [
     CommonModule
   ]
