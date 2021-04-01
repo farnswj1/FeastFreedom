7611fb868bc8adcf1fb269e3c3661e9c670164e1
muhammadsidd <muhammadsidd@mail.usf.edu>
2021-03-31 22:48:06 -0400

    bsdk

---
+++ src/FeastFreedom/src/app/DIservices/providers.ts
@@ -0,0 +1,8 @@
+export class Providers{
+    user: string ='';
+    email: string ='';
+    password:string = '';
+    name:string = '';
+    workdays = ['Sunday', 'Monday', 'Tuesday','Wednesday','Thursday','Friday','Saturday'];
+
+}
