a69bea41acd1ac420aa6bd153209744bc11b99c1
muhammadsidd <muhammadsidd@mail.usf.edu>
2021-04-02 16:27:46 -0400

    templateimproved

--- src/FeastFreedom/src/app/DIservices/kitchen.ts
+++ src/FeastFreedom/src/app/DIservices/kitchen.ts
@@ -1,15 +1,27 @@
-import { Plate } from './plate';
+import { Plate, plate } from './plate';
 import { IKitchenUser } from './providers';
+import { User } from '../users/models/user'
 
 export interface Kitchen {
   id: number;
   user: IKitchenUser;
   name: string;
-  email: string;
   working_days: string[];
   time_in: any;
   time_out: any;
-  image: string;
+  image: any;
   menu: Plate[];
   featured: boolean;
+}
+
+export class kitchen {
+  id: number = 0
+  user = new User();
+  name: string = '';
+  working_days= [""];
+  time_in: any;
+  time_out: any;
+  image: any;
+  menu = new plate();
+  featured: boolean = false;
 }
