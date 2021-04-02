7dc388e237621ed385dba7ca8ce0a41cacae6442
Justin Farnsworth <farnswj1@tcnj.edu>
2021-04-02 10:22:01 -0400

    updated requirements.txt

--- src/FeastFreedom/src/app/app.module.ts
+++ src/FeastFreedom/src/app/app.module.ts
@@ -15,18 +15,25 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { RegularUserListComponent } from './users/components/regular-user-list/regular-user-list.component';
 import { DisplayUserListComponent } from './users/components/display-user-list/display-user-list.component';
 import { InterfaceComponent} from './service/interface/interface.component'
-import { RegisterComponent } from './service/interface/register/register.component'
+import { RegisterComponent } from './service/interface/register/register.component';
+import { KitchenUserListComponent } from './users/components/kitchen-user-list/kitchen-user-list.component'
 
 
 @NgModule({
-  declarations: [AppComponent, RegularUserListComponent, DisplayUserListComponent],
+  declarations: 
+  [
+    AppComponent, 
+    RegularUserListComponent, 
+    DisplayUserListComponent, 
+    KitchenUserListComponent
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     UserModule,
     NgbModule,
-    ServiceModule,
+    //ServiceModule,
     ReactiveFormsModule
   ],
   providers: [],
--- src/FeastFreedom/src/app/users/components/kitchen-user-list/kitchen-user-list.component.ts
+++ src/FeastFreedom/src/app/users/components/kitchen-user-list/kitchen-user-list.component.ts
@@ -2,19 +2,19 @@ import { Component, OnInit } from '@angular/core';
 import { UsersService } from '../../services/users.service';
 
 @Component({
-  selector: 'app-regular-user-list',
-  templateUrl: './regular-user-list.component.html',
-  styleUrls: ['./regular-user-list.component.css']
+  selector: 'app-kitchen-user-list',
+  templateUrl: './kitchen-user-list.component.html',
+  styleUrls: ['./kitchen-user-list.component.css']
 })
-export class RegularUserListComponent implements OnInit {
-  public regularUsers: any;
+export class KitchenUserListComponent implements OnInit {
+  public kitchenUsers: any;
   public errorMsg: any;
 
   constructor(private usersService: UsersService) { }
 
   ngOnInit(): void {
     this.usersService.getRegularUsers().subscribe(
-      (data) => this.regularUsers = data,
+      (data) => this.kitchenUsers = data,
       (error) => this.errorMsg = error,
       () => console.log("Complete!")
     );
