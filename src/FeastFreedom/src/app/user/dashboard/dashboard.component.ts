88776a2935ee74c47821f62be184337997cbb006
Justin Farnsworth <farnswj1@tcnj.edu>
2021-04-07 01:16:21 -0400

    merging Juan's code

--- src/FeastFreedom/src/app/user/dashboard/dashboard.component.ts
+++ src/FeastFreedom/src/app/user/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ProvidersService } from 'src/app/DIservices/providers.service';
+import { KitchensService } from 'src/app/kitchens/services/kitchens.service';
 import { Kitchen } from '../../DIservices/kitchen';
 
 @Component({
@@ -11,17 +12,13 @@ export class DashboardComponent implements OnInit {
   public kitchens: any;
   public error = null;
 
-  constructor(private providersService: ProvidersService) {
-    this.providersService.getKitchen().subscribe(
+  constructor(private kitchensService: KitchensService) {
+    this.kitchensService.getFeaturedKitchens().subscribe(
       (data) => (this.kitchens = data),
       (error) => (this.error = error),
-      () => console.log('Kitchens fetched', this.kitchens)
+      () => console.log('Featured kitchens.')
     );
   }
 
   ngOnInit(): void {}
-
-  getFeatured(): Kitchen[] {
-    return this.kitchens.filter((kitchen: Kitchen) => kitchen.featured);
-  }
 }
