f1567743864ebc7344002701b4482c04aa8dabc7
Juan Leyva <jpleyvat@gmail.com>
2021-04-07 11:01:33 -0400

    Login after register.

--- src/FeastFreedom/src/app/users/components/regular-user-create/regular-user-create.component.ts
+++ src/FeastFreedom/src/app/users/components/regular-user-create/regular-user-create.component.ts
@@ -1,140 +1,187 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { ProvidersService } from 'src/app/DIservices/providers.service';
 import { UsersService } from '../../services/users.service';
 
 @Component({
   selector: 'app-regular-user-create',
   templateUrl: './regular-user-create.component.html',
   styleUrls: ['./regular-user-create.component.css'],
-  providers: [UsersService]
+  providers: [UsersService],
 })
 export class RegularUserCreateComponent implements OnInit {
   public regularUserForm: any;
   public regularUsers: any;
   public errorMsg: any;
 
-  constructor(private fb: FormBuilder, private usersService: UsersService, private router: Router) { }
+  constructor(
+    private fb: FormBuilder,
+    private usersService: UsersService,
+    private router: Router,
+    private providersService: ProvidersService
+  ) {}
 
   ngOnInit(): void {
     this.regularUserForm = this.fb.group({
-      username: ["", [
-        Validators.required, 
-        Validators.minLength(2), 
-        Validators.maxLength(150),
-        Validators.pattern("^[A-Za-z0-9]+$")
-      ]],
-      first_name: ["", [
-        Validators.required, 
-        Validators.minLength(2), 
-        Validators.maxLength(30),
-        Validators.pattern("^[A-Za-z -']+$")
-      ]],
-      last_name: ["", [
-        Validators.required, 
-        Validators.minLength(2), 
-        Validators.maxLength(30),
-        Validators.pattern("^[A-Za-z -']+$")
-      ]],
-      password: ["", [
-        Validators.required, 
-        Validators.minLength(8), 
-        Validators.maxLength(150),
-        Validators.pattern("^.{8,150}$")
-      ]], 
-      email: ["", [
-        Validators.required, 
-        Validators.minLength(8),
-        Validators.maxLength(50),
-        Validators.pattern("[A-Za-z0-9\\._-]+@[A-Za-z0-9\\.-]+\\.[A-Za-z]{2,}")
-      ]],
-      is_active: [true, [
-        Validators.required, 
-      ]],
-      is_staff: [false, [
-        Validators.required, 
-      ]],
-      is_superuser: [false, [
-        Validators.required, 
-      ]],
-      date_joined: [new Date(), [
-        Validators.required, 
-      ]],
+      username: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(2),
+          Validators.maxLength(150),
+          Validators.pattern('^[A-Za-z0-9]+$'),
+        ],
+      ],
+      first_name: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(2),
+          Validators.maxLength(30),
+          Validators.pattern("^[A-Za-z -']+$"),
+        ],
+      ],
+      last_name: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(2),
+          Validators.maxLength(30),
+          Validators.pattern("^[A-Za-z -']+$"),
+        ],
+      ],
+      password: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(8),
+          Validators.maxLength(150),
+          Validators.pattern('^.{8,150}$'),
+        ],
+      ],
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(8),
+          Validators.maxLength(50),
+          Validators.pattern(
+            '[A-Za-z0-9\\._-]+@[A-Za-z0-9\\.-]+\\.[A-Za-z]{2,}'
+          ),
+        ],
+      ],
+      is_active: [true, [Validators.required]],
+      is_staff: [false, [Validators.required]],
+      is_superuser: [false, [Validators.required]],
+      date_joined: [new Date(), [Validators.required]],
       last_login: [null, []],
-      phone_number: ["", [
-        Validators.required, 
-        Validators.minLength(3), 
-        Validators.maxLength(12),
-        Validators.pattern("^\\d{3}-\\d{3}-\\d{4}$")
-      ]],
-      address: ["", [
-        Validators.required,
-        Validators.minLength(3),
-        Validators.maxLength(40),
-        Validators.pattern("^\\d{1,5} [a-zA-Z0-9\\. ]+$")
-      ]],
-      city: ["", [
-        Validators.required,
-        Validators.minLength(2),
-        Validators.maxLength(25),
-        Validators.pattern("^[a-zA-Z\\. -']+$")
-      ]],
-      state: ["", [
-        Validators.required,
-        Validators.minLength(2),
-        Validators.maxLength(2),
-        Validators.pattern("^[A-Z]{2}$")
-      ]],
-      zip_code: ["", [
-        Validators.required,
-        Validators.minLength(5),
-        Validators.maxLength(10),
-        Validators.pattern("^\\d{5}(-\\d{4})?$")
-      ]],
-      question_one: ["", [
-        Validators.required,
-        Validators.minLength(20),
-        Validators.maxLength(80),
-        Validators.pattern("^[A-Za-z0-9 ,'//]{20,80}\\?$")
-      ]],
-      answer_one: ["", [
-        Validators.required,
-        Validators.minLength(2),
-        Validators.maxLength(50),
-        Validators.pattern("^[A-Za-z0-9 ,'-:]{2,50}$")
-      ]],
-      question_two: ["", [
-        Validators.required,
-        Validators.minLength(20),
-        Validators.maxLength(80),
-        Validators.pattern("^[A-Za-z0-9 ,'//]{20,80}\\?$")
-      ]],
-      answer_two: ["", [
-        Validators.required,
-        Validators.minLength(2),
-        Validators.maxLength(50),
-        Validators.pattern("^[A-Za-z0-9 ,'-:]{2,50}$")
-      ]],
+      phone_number: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(3),
+          Validators.maxLength(12),
+          Validators.pattern('^\\d{3}-\\d{3}-\\d{4}$'),
+        ],
+      ],
+      address: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(3),
+          Validators.maxLength(40),
+          Validators.pattern('^\\d{1,5} [a-zA-Z0-9\\. ]+$'),
+        ],
+      ],
+      city: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(2),
+          Validators.maxLength(25),
+          Validators.pattern("^[a-zA-Z\\. -']+$"),
+        ],
+      ],
+      state: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(2),
+          Validators.maxLength(2),
+          Validators.pattern('^[A-Z]{2}$'),
+        ],
+      ],
+      zip_code: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(5),
+          Validators.maxLength(10),
+          Validators.pattern('^\\d{5}(-\\d{4})?$'),
+        ],
+      ],
+      question_one: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(20),
+          Validators.maxLength(80),
+          Validators.pattern("^[A-Za-z0-9 ,'//]{20,80}\\?$"),
+        ],
+      ],
+      answer_one: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(2),
+          Validators.maxLength(50),
+          Validators.pattern("^[A-Za-z0-9 ,'-:]{2,50}$"),
+        ],
+      ],
+      question_two: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(20),
+          Validators.maxLength(80),
+          Validators.pattern("^[A-Za-z0-9 ,'//]{20,80}\\?$"),
+        ],
+      ],
+      answer_two: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(2),
+          Validators.maxLength(50),
+          Validators.pattern("^[A-Za-z0-9 ,'-:]{2,50}$"),
+        ],
+      ],
       groups: [[], []],
-      user_permissions: [[], []]
+      user_permissions: [[], []],
     });
   }
 
-  onSubmit(regularUserForm: any){
+  onSubmit(regularUserForm: any) {
     this.usersService.createRegularUser(this.regularUserForm.value).subscribe(
       (data) => {
         this.regularUsers = data;
         this.usersService.getRegularUsers().subscribe(
           (data) => {
             this.regularUsers = data;
-            this.router.navigate(['/users']);
+            this.providersService.logIn(
+              this.regularUserForm.value.username,
+              this.regularUserForm.value.password
+            );
+            // this.
+            this.router.navigate(['/']);
             this.regularUserForm.reset();
           },
-          (error) => this.errorMsg = error
-        )
+          (error) => (this.errorMsg = error)
+        );
       },
-      (error) => this.errorMsg = error
-    )
+      (error) => (this.errorMsg = error)
+    );
   }
 
   get username() {
@@ -221,7 +268,7 @@ export class RegularUserCreateComponent implements OnInit {
     return this.regularUserForm.get('user_permissions');
   }
 
-  regularUsersList(){
-    this.router.navigate(['/users'])
+  regularUsersList() {
+    this.router.navigate(['/users']);
   }
 }
